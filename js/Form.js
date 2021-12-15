edcaaecd90c391254ded252d7504c05a6cd94050
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-12-15 16:47:16 -0300

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -2,7 +2,7 @@ class Form {
   constructor() {
     this.input = createInput("").attribute("placeholder", "Digite Seu Nome");
     this.playButton = createButton("Jogar");
-    this.titleImg = createImg("./assets/title.png", "game title");
+    this.titleImg = createImg("./assets/TITULO.png", "game title");
     this.greeting = createElement("h2");
   }
 
