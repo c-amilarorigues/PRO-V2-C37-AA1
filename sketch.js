ad5eeef080541844754773b3f5bb65fdec2fb040
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-12-17 10:12:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
   car1_img = loadImage("../assets/car1.png");
   car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
+  track = loadImage("../assets/pista.png");
 }
 
 function setup() {
